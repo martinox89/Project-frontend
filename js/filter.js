a153147d3ec51c487151259beed4fb73db429620
Martijn <martijn--smit@hotmail.com>
2019-10-27 22:46:27 +0100

    Updated
    updated

--- js/filter.js
+++ js/filter.js
@@ -1,9 +1,27 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 var Knop = document.querySelector('button');
 var Form = document.querySelector('form');
+var More = document.querySelector('.wow');
+var Web = document.querySelector('body');
+var Hide = document.querySelector('#hide');
+var Knop2 = document.querySelector('#knop');
 
-function filteren () {
-Form.classList.toggle('show');
+function filteren() {
+  Form.classList.toggle('show');
 }
 
-Knop.addEventListener('click', filteren); 
+function foetsie() {
+  Hide.classList.toggle('tada');
+}
+
+function raar() {
+  Web.classList.toggle('raar');
+}
+
+Knop2.addEventListener('click', foetsie);
+
+More.addEventListener('click', raar);
+
+
+
+Knop.addEventListener('click', filteren);
