4f550a39ea786a89a3d711bc62d81824c5f54b69
Martijn <martijn--smit@hotmail.com>
2019-10-30 11:41:18 +0100

    Front end opdracht

--- js/filter.js
+++ js/filter.js
@@ -1,27 +1,39 @@
-/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true */
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+/*global Audio: false */
+
+//BRON: In de les besproken code 
+
 var Knop = document.querySelector('button');
 var Form = document.querySelector('form');
 var More = document.querySelector('.wow');
 var Web = document.querySelector('body');
-var Hide = document.querySelector('#hide');
+var Hide = document.querySelector('.hide');
 var Knop2 = document.querySelector('#knop');
+var audio = new Audio();
+audio.src = "content/nogodno.mp3";
+
+//verschillende fuctie aangemaakt die gaan werken wanneer de functie wordt aangeroepen
 
 function filteren() {
   Form.classList.toggle('show');
 }
 
 function foetsie() {
-  Hide.classList.toggle('tada');
+  Hide.classList.toggle('daarisdie');
 }
 
 function raar() {
   Web.classList.toggle('raar');
+  audio.play();
 }
+
+//Classlist Toggle zorgt ervoor dat er een class aangemaakt wordt met de waarde die de jijzelf bepaalt.
+
 
 Knop2.addEventListener('click', foetsie);
 
 More.addEventListener('click', raar);
-
-
 
 Knop.addEventListener('click', filteren);
